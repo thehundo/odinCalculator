be90525883ff37bb795a00f5db918645e65d585a
Brandon Hughes <157261373+thehundo@users.noreply.github.com>
2024-04-13 15:09:50 -0400

    Create and test operator functions

---
+++ script.js
@@ -0,0 +1,17 @@
+const addNums = (a, b) => a + b;
+const subNums = (a, b) => a - b;
+const multNums = (a, b) => a * b;
+const divNums = (a, b) => a / b;
+
+let a = 47
+let b = 3
+
+
+
+
+console.log(addNums(a, b));
+console.log(subNums(a, b));
+console.log(multNums(a, b));
+console.log(divNums(a, b));
+
+
