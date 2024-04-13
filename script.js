8bae89f4b767e22250298fe0d3f51ff4e1e86d85
Brandon Hughes <157261373+thehundo@users.noreply.github.com>
2024-04-13 15:52:40 -0400

    Create and test operate function

--- script.js
+++ script.js
@@ -5,13 +5,28 @@ const divNums = (a, b) => a / b;
 
 let a = 47
 let b = 3
+let operator = 'mul'
+
+const calculator = {
+    add: addNums(a, b),
+    sub: subNums(a, b),
+    mul: multNums(a, b),
+    div: divNums(a, b),
+}
 
 
+console.log(operate(a, b, operator));
 
 
-console.log(addNums(a, b));
-console.log(subNums(a, b));
-console.log(multNums(a, b));
-console.log(divNums(a, b));
+function operate(a, b, operator) {
+    let key = operator;
+    return calculator[key]
+}
+
+
+// console.log(addNums(a, b));
+// console.log(subNums(a, b));
+// console.log(multNums(a, b));
+// console.log(divNums(a, b));
 
 
