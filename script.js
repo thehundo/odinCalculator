d3fcc6dfd9b9ff3f63f291294dc66e1937c31e45
Brandon Hughes <157261373+thehundo@users.noreply.github.com>
2024-04-16 17:28:07 -0400

    Wired operate function for full bare bones functionality

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 let a = ''
 let b = ''
-let opVal = ''
-let operator = 'mul'
+// let opVal = ''
+let operator = ''
 
 const addNums = (a, b) => a + b;
 const subNums = (a, b) => a - b;
@@ -16,12 +16,8 @@ const calculator = {
 }
 
 // Store user input
-const userInput = []
-
-
-
+const userInput = [] 
 const container = document.querySelector("#container");
-
 // Create display div to show result and add DOM
 const display = document.createElement("div");
 display.classList.add("display");
@@ -33,36 +29,66 @@ const calc = document.createElement("div");
 calc.classList.add("calc");
 container.appendChild(calc);
 
+// Build Calculator
+makeCalcLeft();
+makeCalcRight();
 
-// Create divs for left and right of calculator
-const calcLeft = document.createElement("div");
-calcLeft.classList.add("calcLeft");
-calc.appendChild(calcLeft);
-makeButton("numBtn", "00", calcLeft);
-makeButton("numBtn", "0", calcLeft);
-makeButton("numBtn", ".", calcLeft);
-makeButton("numBtn", "1", calcLeft);
-makeButton("numBtn", "2", calcLeft);
-makeButton("numBtn", "3", calcLeft);
-makeButton("numBtn", "4", calcLeft);
-makeButton("numBtn", "5", calcLeft);
-makeButton("numBtn", "6", calcLeft);
-makeButton("numBtn", "7", calcLeft);
-makeButton("numBtn", "8", calcLeft);
-makeButton("numBtn", "9", calcLeft);
-makeButton("spBtn", "C", calcLeft);
+function operate() {
+    const calculator = {
+        add: addNums(a, b),
+        sub: subNums(a, b),
+        mul: multNums(a, b),
+        div: divNums(a, b),
+    };
+    //storeValue();
+    let key = operator;
+    console.log(a, key, b);
+    // console.log(calculator['add']);
+    a = calculator[key];
+    b = '';
+    display.textContent = a;
+}
 
+// Create calculator display
+function makeDisplay() {
 
+}
 
-const calcRight = document.createElement("div");
-calcRight.classList.add("calcRight");
-calc.appendChild(calcRight);
-makeButton("opBtn", "÷", calcRight);
-makeButton("opBtn", "×", calcRight);
-makeButton("opBtn", "-", calcRight);
-makeButton("opBtn", "+", calcRight);
-makeButton("spBtn", "=", calcRight);
+// Create left side of Calculator
+function makeCalcLeft() {
+    const calcLeft = document.createElement("div");
+    calcLeft.classList.add("calcLeft");
+    calc.appendChild(calcLeft);
+    makeButton("numBtn", "00", calcLeft);
+    makeButton("numBtn", "0", calcLeft);
+    makeButton("numBtn", ".", calcLeft);
+    makeButton("numBtn", "1", calcLeft);
+    makeButton("numBtn", "2", calcLeft);
+    makeButton("numBtn", "3", calcLeft);
+    makeButton("numBtn", "4", calcLeft);
+    makeButton("numBtn", "5", calcLeft);
+    makeButton("numBtn", "6", calcLeft);
+    makeButton("numBtn", "7", calcLeft);
+    makeButton("numBtn", "8", calcLeft);
+    makeButton("numBtn", "9", calcLeft);
+    makeButton("spBtn", "C", calcLeft)
+}
 
+// Create right side of Calculator
+function makeCalcRight() {
+    const calcRight = document.createElement("div");
+    calcRight.classList.add("calcRight");
+    calc.appendChild(calcRight);
+    makeButton("opBtn", "div", calcRight);
+    makeButton("opBtn", "mul", calcRight);
+    makeButton("opBtn", "sub", calcRight);
+    makeButton("opBtn", "add", calcRight);
+    // makeButton("opBtn", "÷", calcRight);
+    // makeButton("opBtn", "×", calcRight);
+    // makeButton("opBtn", "-", calcRight);
+    // makeButton("opBtn", "+", calcRight);
+    makeButton("equalBtn", "=", calcRight);
+}
 
 // Create buttons
 function makeButton(cls, val, parent) {
@@ -73,6 +99,7 @@ function makeButton(cls, val, parent) {
     addBtnClick(btn, cls, val)
 }
 
+// Add button clicks for each class
 function addBtnClick(btn, cls, val) {
     btn.addEventListener("click", function (e) {
         switch (cls) {
@@ -82,40 +109,42 @@ function addBtnClick(btn, cls, val) {
                 break;
             case 'opBtn':
                 storeValue();
-                opVal = val;
-                e.target.style.back
+                return operator = val;
+                console.log(a, operator, b);
                 break;
-            case 'spBtn': 
-                clearAll();
+            // case 'spBtn': 
+            //     clearAll();
+            //     break;
+            case 'equalBtn':
+                storeValue()
+                operate();
                 break;
 
         }
     })
 }
 
+// Create function to clear all memory (userInput, a, b)
 function clearAll() {
     a = ''
     b = ''
     userInput.length = 0;
-    display.textContent = userInput.join()
+    display.textContent = userInput.join() // update display (to blank)
 }
 
+// Store userInput as a (if empty) or b
 function storeValue() {
-    if (a === '') {a = userInput.join('')}
-        else {b = userInput.join('')};
-    userInput.length = 0
-    console.log(a)
-    console.log(b)
+    if (a === '') {a = parseInt(userInput.join(''))}
+    else {b = parseInt(userInput.join(''))};
+    userInput.length = 0;
+    console.log(`a: ${a}  operator: ${operator}  b: ${b}`) // log current values for operate function
 }
 
-console.log(calculator.mul)
-console.log(operate(a, b, operator));
+
+// console.log(calculator.mul)
+// console.log(operate(a, b, operator));
 
 
-function operate(a, b, operator) {
-    let key = operator;
-    return calculator[key]
-}
 
 
 // console.log(addNums(a, b));
